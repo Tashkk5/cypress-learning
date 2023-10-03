6a77a35b0431b7c4ffc5a266f10098c1ce78f5ed
Tashfeen <tashfeenamin@outlook.com>
2023-10-03 14:54:12 +0100

    updating code

--- cypress/e2e/lesson1.cy.js
+++ cypress/e2e/lesson1.cy.js
@@ -1,15 +1,16 @@
 /// <reference types="Cypress" />
+
 describe('Login functionality of ThingsHub', () => {
 
   beforeEach('', () => {
     cy.viewport('macbook-16');
-    cy.visit('localhost:4200/ui/login');
+    cy.visit('https://dev.thingshub.smartmakers.de/ui/');
   });
-  
+
   it('should open the thinghub login page', () => {
     const myName = 'Tashfeeen';
 
-    myName.slice(1,5)
+    myName.slice(1, 5)
   })
 
   it('should show an error message if we login with empty username', () => {
@@ -64,7 +65,6 @@ describe('Login functionality of ThingsHub', () => {
     passwordElement.type(invalidPassword)
     loginButton.click();
 
-    cy.wait(2000);
 
     //match expectation
     const errorMessageElement = cy.get('.alert.alert-danger.th-mt-4.th-mb-4.ng-star-inserted');
@@ -89,11 +89,48 @@ describe('Login functionality of ThingsHub', () => {
     loginButton.click();
 
     // match expectations
-    cy.wait(2000);
     const userNamePlaceholder = cy.get('.mat-menu-trigger.text-decoration-none');
     userNamePlaceholder.should('contain.text', 'Tashfeen');
-    cy.url().should('contain.text', expectedMatchingUrl);
+
+    cy.url().then((url) => {
+      expect(url).contains(expectedMatchingUrl);
+    })
   });
 
+  it('should apply the right language after login', () => {
+    // test data
+    const expectedIndexOfGermanLanguage = 1;
+    const expectedCurrentLanguageSelectionInUrl = '/de/';
+    const expectedSelectedLanguageValue = 'de'
+    // elements
+    const languageSelectionDropdownElement = cy.get('.translate-wrap > .form-control');
+    const languageChangeButtonElement = cy.get('.btn-sm > div > .btn > span');
+
+    // actions
+    languageSelectionDropdownElement.select(expectedIndexOfGermanLanguage);
+    languageChangeButtonElement.click();
+
+    // expectations
+    cy.url().then((value) => {
+      expect(value).contain(expectedCurrentLanguageSelectionInUrl);
+    });
+  
+    languageSelectionDropdownElement.then((element) => {
+      expect(element['0'].value).equal(expectedSelectedLanguageValue);
+    });
+  
+    
+    cy.reload();
+
+    cy.url().then((value) => {
+      expect(value).contain(expectedCurrentLanguageSelectionInUrl);
+    });
+  
+    languageSelectionDropdownElement.then((element) => {
+      expect(element['0'].value).equal(expectedSelectedLanguageValue);
+    });
+    
+  });
 
 })
+
