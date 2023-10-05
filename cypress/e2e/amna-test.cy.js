2d6ca1534939bc38527b2332012732548ab1eccf
Tashfeen <tashfeenamin@outlook.com>
2023-10-05 14:22:37 +0100

    feedback

--- cypress/e2e/amna-test.cy.js
+++ cypress/e2e/amna-test.cy.js
@@ -1,89 +1,73 @@
 /// <reference types="Cypress" />
 
+/*  ----------------
+- No sections in first case, as per golden rules
+*/
+
+
 describe('Bind Asset Tracker Functionality', () => {
-    
+
     beforeEach('', () => {
         cy.viewport('macbook-16');
         cy.visit('https://dev.thingshub.smartmakers.de/ui/');
+        loginIntoThingsHub();
+    });
 
-      });
+    it(`should open bind dialog on clicking "Bind Asset Tracker" button`, () => {
+        // test elements
+        const bindAssetTrackerButtonElement = cy.get(':nth-child(2) > thub-button > div.ng-star-inserted > .btn');
 
-      it('should navigate to home page after login with correct credentials', () => {
-        // test data
-        const validUsername = 'tashfeen';
-        const validPassword = 'leobardeen';
-        const expectedMatchingUrl = '/home';
-    
-        // elements of test case
-        const usernameElement = cy.get('#username');
-        const passwordElement = cy.get('#password');
-        const loginButton = cy.get(`button[class='btn btn-block btn-primary']`);
-        
-    
         // actions
-        usernameElement.type(validUsername);
-        passwordElement.type(validPassword);
-        loginButton.click();
+        bindAssetTrackerButtonElement.click();
 
-        const bindAssetTrackerButton = cy.get(':nth-child(2) > thub-button > div.ng-star-inserted > .btn');
-        bindAssetTrackerButton.click();
-
+        // matching expectations
         const dialogueBox = cy.get('.modal-body');
         dialogueBox.should('exist');
 
         const dialogueBoxTitle = cy.get('.m-0');
-        dialogueBoxTitle.should('contain.text', 'Bind Asset with Asset Tracker or Asset Tag' );
-
-        const dialogueBoxCancelButton = cy.get('.mr-2 > div.ng-star-inserted > .btn');
-        dialogueBoxCancelButton.click();
-        dialogueBox.should('not.exist');
-
-        });
-
-    it('should load 15 more assets in the table when "Load More" button is clicked',() => {
-        
-        // test data
-        const validUsername = 'tashfeen';
-        const validPassword = 'leobardeen';
-        const expectedMatchingUrl = '/home';
-    
-         // Test Data
-         const initialCount = 15;
-         const increamentedCount = 30;
-
-        // elements of test case
-        const usernameElement = cy.get('#username');
-        const passwordElement = cy.get('#password');
-        const loginButton = cy.get(`button[class='btn btn-block btn-primary']`);
-        
-    
-        // actions
-        usernameElement.type(validUsername);
-        passwordElement.type(validPassword);
-        loginButton.click();
-
-
-        // Test Elements
-        const loadMoreButton = cy.get('.w-100 > .d-flex');
-        loadMoreButton.should('exist');
-
-        const itemsCount = cy.get('.w-100 > :nth-child(2) > span');
-        itemsCount.should('contain.text','15');
-        
-        loadMoreButton.click();
-        
-        const newItemsCount= cy.get('.w-100 > :nth-child(2) > span');
-        newItemsCount.should('contain.text','30');
-
-        // Match Expectations
-        // for (let i = 0; i < 5; i++) {
-
-        //     newValue = itemsCountValue + increamentedValue;
-        //     itemsCount.should('equal', newValue);
-        //     itemsCountValue = newValue;
-        //     loadMoreButton.click();
-
-        // }
+        dialogueBoxTitle.should('contain.text', 'Bind Asset with Asset Tracker or Asset Tag');
     });
+
+    it(`should increase the table counter after load more`, () => {
+        // test data
+        const expectedInitialTableCount = 15;
+        const expectedTableCountAfterLoadMore = 30;
+
+        // test elements
+        const loadMoreButtonElement = cy.get('.w-100 > .d-flex');
+
+
+        // matching expectations
+        loadMoreButtonElement.should('exist');
+
+        const tableItemsCounterElement = cy.get('.w-100 > :nth-child(2) > span');
+        tableItemsCounterElement.should('contain.text', expectedInitialTableCount);
+
+        loadMoreButtonElement.click();
+
+        const newItemsCount = cy.get('.w-100 > :nth-child(2) > span');
+        newItemsCount.should('contain.text', expectedTableCountAfterLoadMore);
+    });
+
+    
 });
+
+
+function loginIntoThingsHub() {
+    // test data
+    const validUsername = 'tashfeen';
+    const validPassword = 'leobardeen';
+    const expectedMatchingUrl = '/home';
+
+    // elements of test case
+    const usernameElement = cy.get('#username');
+    const passwordElement = cy.get('#password');
+    const loginButton = cy.get(`button[class='btn btn-block btn-primary']`);
+
+
+    // actions
+    usernameElement.type(validUsername);
+    passwordElement.type(validPassword);
+    loginButton.click();
+}
 
